48a5cda20b5b91b547175251459f7932074d7d50
Natalya Ryzhikh <natalya.ryzhikh@umbrellait.com>
2024-09-28 14:20:02 +0300

    Решение для задачи createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,22 @@
-import { defineComponent, createApp } from 'vue'
+import {defineComponent, createApp} from 'vue'
+
+const App = defineComponent({
+  name: 'DataVApp',
+  setup() {
+    const localDate = (new Date()).toLocaleDateString(navigator.language, {dateStyle: 'long'});
+
+    return {
+      localDate
+    }
+  },
+
+  template: `
+    <div>Сегодня {{ localDate }}</div>
+  `,
+})
+
+const app = createApp(App)
+
+const vm = app.mount('#app')
+
+window.vm = vm
